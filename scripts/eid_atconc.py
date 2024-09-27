da7859481c8ee6e7be1aaf8921cbf3b32b5e2ed0
Derek Johnston <74471900+derek-johnston@users.noreply.github.com>
2024-09-27 16:40:41 -0500

    Reading-in 15mm and 150mm datasets.

--- scripts/eid_atconc.py
+++ scripts/eid_atconc.py
@@ -25,5 +25,23 @@ def eid_atconc():
     mgcl2_150um  = get_data(f"{root}mgcl2.csv",  label="mgcl2")
     nacl_150um   = get_data(f"{root}nacl.csv",   label="nacl")
     nano3_150um  = get_data(f"{root}nano3.csv",  label="nano3")
+    #==========================================================================
+    #   Import 15mm dataset.
+    #==========================================================================
+    root = "data/electrolyte/electrolyte_15mm_"
+    kcl_15mm    = get_data(f"{root}kcl.csv",    label="kcl")
+    kno3_15mm   = get_data(f"{root}kno3.csv",   label="kno3")
+    mgcl2_15mm  = get_data(f"{root}mgcl2.csv",  label="mgcl2")
+    nacl_15mm   = get_data(f"{root}nacl.csv",   label="nacl")
+    nano3_15mm  = get_data(f"{root}nano3.csv",  label="nano3")
+    #==========================================================================
+    #   Import 150mm dataset
+    #==========================================================================
+    root = "data/electrolyte/electrolyte_150mm_"
+    kcl_150mm    = get_data(f"{root}kcl.csv",    label="kcl")
+    kno3_150mm   = get_data(f"{root}kno3.csv",   label="kno3")
+    mgcl2_150mm  = get_data(f"{root}mgcl2.csv",  label="mgcl2")
+    nacl_150mm   = get_data(f"{root}nacl.csv",   label="nacl")
+    nano3_150mm  = get_data(f"{root}nano3.csv",  label="nano3")
 #==============================================================================
 if __name__ == "__main__": eid_atconc()
