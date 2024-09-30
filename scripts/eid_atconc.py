e0523e623182e55782bdee9bc11d235e442b46b5
Derek Johnston <74471900+derek-johnston@users.noreply.github.com>
2024-09-30 10:47:05 -0500

    Generated Figure 1 for electrolyte classification at all concentrations.

--- scripts/eid_atconc.py
+++ scripts/eid_atconc.py
@@ -1,9 +1,9 @@
 #==============================================================================
 #   Dependencies
 #==============================================================================
-import matplotlib.pyplot    as plt
-import pandas               as pd
-from helpers                import get_data
+import matplotlib.pyplot        as plt
+import pandas                   as pd
+from helpers                    import get_data, gen_model, plot_cm
 #==============================================================================
 def eid_atconc():
     """Classify electrolytes at specific concentrations"""
@@ -43,5 +43,58 @@ def eid_atconc():
     mgcl2_150mm  = get_data(f"{root}mgcl2.csv",  label="mgcl2")
     nacl_150mm   = get_data(f"{root}nacl.csv",   label="nacl")
     nano3_150mm  = get_data(f"{root}nano3.csv",  label="nano3")
+    #==========================================================================
+    #   # Compile the datasets
+    #==========================================================================
+    data_15um   = pd.concat([
+        kcl_15um, kno3_15um, mgcl2_15um, nacl_15um, nano3_15um
+    ]).sample(frac=1)
+    data_150um  = pd.concat([
+        kcl_150um, kno3_150um, mgcl2_150um, nacl_150um, nano3_150um
+    ]).sample(frac=1)
+    data_15mm   = pd.concat([
+        kcl_15mm, kno3_15mm, mgcl2_15mm, nacl_15mm, nano3_15mm
+    ]).sample(frac=1)
+    data_150mm  = pd.concat([
+        kcl_150mm, kno3_150mm, mgcl2_150mm, nacl_150mm, nano3_150mm
+    ]).sample(frac=1)
+    #==========================================================================
+    #   Generate models
+    #==========================================================================
+    cm_15um     = gen_model(data_15um)
+    cm_150um    = gen_model(data_150um)
+    cm_15mm     = gen_model(data_15mm)
+    cm_150mm    = gen_model(data_150mm)
+    #==========================================================================
+    #   Generate Confusion Matrix Plots
+    #==========================================================================
+    fig , axes = plt.subplots(1, 4, figsize=(10, 3.25))
+    # Set the font size
+    plt.rcParams.update({"font.size": 9})
+    # Plot the confusion matrices
+    plot_cm(cm_15um,    axes[0])
+    plot_cm(cm_150um,   axes[1])
+    plot_cm(cm_15mm,    axes[2])
+    plot_cm(cm_150mm,   axes[3])
+    # Remove the axis labels
+    axes[0].set_xlabel("")
+    axes[0].set_ylabel("")
+    axes[1].set_xlabel("")
+    axes[1].set_ylabel("")
+    axes[2].set_xlabel("")
+    axes[2].set_ylabel("")
+    axes[3].set_xlabel("")
+    axes[3].set_ylabel("")
+    # Remove the yticks from the right figures
+    axes[1].set_yticks([],[])
+    axes[2].set_yticks([],[])
+    axes[3].set_yticks([],[])
+    # Set some titles
+    axes[0].set_title("(a)")
+    axes[1].set_title("(b)")
+    axes[2].set_title("(c)")
+    axes[3].set_title("(d)")
+    plt.tight_layout()
+    plt.savefig("images/eid_atconc.svg")
 #==============================================================================
 if __name__ == "__main__": eid_atconc()
--- scripts/helpers.py
+++ scripts/helpers.py
@@ -1,15 +1,43 @@
 #==============================================================================
 #   Dependencies
 #==============================================================================
-from pandas import read_csv
+from numpy.random               import normal
+from pandas                     import read_csv
+from sklearn.neighbors          import KNeighborsClassifier
+from sklearn.model_selection    import train_test_split
+from sklearn.metrics            import confusion_matrix, ConfusionMatrixDisplay
 #==============================================================================
-def get_data(path, label=""):
+def get_data(path, label="", noise=True):
     """Read-in and parse an HFTS dataset."""
     data = read_csv(path)
     data = data.drop("Time", axis=1)
     data = data.astype(complex)
     data = data.apply(abs)
+    if noise:
+        data += normal(0, 0.0025, data.shape)
     if label != "":
-        data["Label"] = label
+        data["label"] = label
     return data
+#==============================================================================
+def gen_model(dataset):
+    """Generate a KNN model from the given dataset"""
+    X = dataset.drop("label", axis=1).to_numpy()
+    y = dataset["label"].to_numpy()
+    # Train the model
+    X_train, X_test, y_train, y_test = train_test_split(X, y)
+    model = KNeighborsClassifier()
+    model.fit(X_train, y_train)
+    print(f"Model Trained with score = {model.score(X_test, y_test)}")
+    # Generate and return the confusion matrix
+    labels = list(set(y))
+    y_pred = model.predict(X_test)
+    matrix = confusion_matrix(y_test, y_pred)
+    return ConfusionMatrixDisplay(
+        confusion_matrix=matrix, 
+        display_labels=labels
+    )
+#==============================================================================
+def plot_cm(cm, axes):
+    """Generate a confusion matrix plot"""
+    cm.plot(ax=axes, cmap="Greys", colorbar=False, xticks_rotation="vertical")
 #==============================================================================
