494b807b860d3df5ec21722270dacce43ecd5040
Derek Johnston <74471900+derek-johnston@users.noreply.github.com>
2024-10-01 10:59:35 -0500

    Adding Figure 4, electrolyte anomaly detection.

--- scripts/elec_anomaly.py
+++ scripts/elec_anomaly.py
@@ -4,7 +4,7 @@
 import  matplotlib.pyplot   as plt
 from    numpy               import where, sum, invert
 from    pandas              import concat
-from    helpers             import get_data
+from    helpers             import get_data, detect_anomalies
 #==============================================================================
 def elec_anomaly():
     """Detect anomalous samples in electrolyte data"""
@@ -19,49 +19,57 @@ def elec_anomaly():
     #   combine the dataset with anomalies
     #==========================================================================
     data = concat([
-        kcl_150mm.sample(frac=0.9),
-        nacl_150mm.sample(frac=0.1)
+        kcl_150mm.sample(frac=0.99),
+        nacl_150mm.sample(frac=0.01)
     ]).sample(frac=1)
+    print(data.shape)
     #==========================================================================
     #   Perform anomaly detection
     #==========================================================================
-    X = data.drop("label", axis=1).to_numpy()
-    y = data["label"].to_numpy()
-    model = NearestNeighbors()
-    model.fit(X)
-    distances, _ = model.kneighbors(X)
-    plt.plot(distances.mean(axis=1))
-    anomaly_idxs = where(distances.mean(axis=1) > 0.0019)
-    nominal_idxs = where(distances.mean(axis=1) <= 0.0019)
-    anomalies = data.iloc[anomaly_idxs]
-    nominals = data.iloc[nominal_idxs]
-    indexes = anomaly_idxs[0]
-    anoms = []
-    noms = []
-    anom_correct = []
-    noms_correct = []
-    for i in anomaly_idxs[0]:
-        label = data.iloc[i]["label"]
-        if label == "nacl": anom_correct.append(True)
-        else: anom_correct.append(False)
-        anoms.append(distances.mean(axis=1)[i])
-    for i in nominal_idxs[0]:
-        label = data.iloc[i]["label"]
-        if label == "kcl": noms_correct.append(True)
-        else: noms_correct.append(False)
-        noms.append(distances.mean(axis=1)[i])
-    print(100*"*")
-    print("ANOMALY DETECTION COMPLETE")
-    print(f"True Positive Rate: {round(100*sum(anom_correct)/len(anom_correct))}%")
-    print(f"False Positive Rate: {round(100*sum(invert(anom_correct))/len(anom_correct))}%")
-    print(f"True Negative Rate: {round(100*sum(noms_correct)/len(noms_correct))}%")
-    print(f"False Negative Rate: {round(100*sum(invert(noms_correct))/len(noms_correct))}%")
+    results = detect_anomalies(data, threshold=0.002)
     #==========================================================================
     #   Plot the results
     #==========================================================================
-    _, axes = plt.subplots(1, 1, figsize=(3.25, 3.25))
-   # axes.scatter(indexes, di)
-    plt.savefig("images/elec_anomaly.png")
-
+    
+    fig, axes = plt.subplots(1, 2, figsize=(10, 4))
+    plt.rcParams.update({"font.size": 9})
+    # Confusion Matrix
+    cm = [[results["n_tp"], results["n_fp"]], [results["n_fn"], results["n_tn"]]]
+    axes[1].imshow(cm, cmap="Greys")
+    for i in range(2):
+        for j in range(2):
+            if cm[i][j] > 50: color="white"
+            else: color="black"
+            axes[1].annotate(f"{str(cm[i][j])}%", xy=(j, i), ha="center", va="center", color=color, fontsize=16)
+    axes[1].set_title("(b)")
+    axes[1].set_yticks([0, 1], ["Normal", "Anomaly"])
+    axes[1].set_xticks([0, 1], ["Normal", "Anomaly"])
+    axes[1].set_ylabel("Predicted")
+    axes[1].set_xlabel("Actual")
+    # Scatter Plot
+    color_true = "black"
+    color_false = "dimgray"
+    for r, i in zip(results["t_pos"], range(len(results["t_pos"]))):
+        if i == 0: axes[0].scatter(r[0], r[1], color=color_true, marker="*", label="True Positive")
+        else: axes[0].scatter(r[0], r[1], color=color_true, marker="*")
+    for r, i in zip(results["t_neg"], range(len(results["t_neg"]))):
+        if i == 0: axes[0].scatter(r[0], r[1], color=color_false, marker="*", label="True Negative")
+        else: axes[0].scatter(r[0], r[1], color=color_false, marker="*")
+    for r, i in zip(results["f_pos"], range(len(results["f_pos"]))):
+        if i == 0: axes[0].scatter(r[0], r[1], color=color_true, marker="x", label="False Positive")
+        else: axes[0].scatter(r[0], r[1], color=color_true, marker="x")
+    for r, i in zip(results["f_neg"], range(len(results["f_neg"]))):
+        if i == 0: axes[0].scatter(r[0], r[1], color=color_false, marker="x", label="False Negative")
+        else: axes[0].scatter(r[0], r[1], color=color_false, marker="x")
+    
+    axes[0].legend(frameon=False)
+    axes[0].set_ylabel("Average Distance")
+    axes[0].set_xlabel("Sample Index")
+    axes[0].set_xlim([0, 1000])
+    axes[0].set_ylim([0.0009, 0.0040])
+    axes[0].set_title("(a)")
+    plt.tight_layout()
+    plt.savefig("images/elec_anomaly.svg")
+    
 #==============================================================================
 if __name__ == "__main__": elec_anomaly()
