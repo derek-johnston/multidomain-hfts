7401c9312e10471f4d1ff14524bc81c126ba540a
Derek Johnston <derek.a.johnston@ttu.edu>
2024-10-03 12:21:06 -0500

    Adding script for reading-in and storing semiconductor data.

---
+++ scripts/process_semi_data.py
@@ -0,0 +1,33 @@
+#==============================================================================
+#   Dependencies
+#==============================================================================
+from os     import listdir
+from pandas import read_csv, DataFrame
+from pickle import dump
+#==============================================================================
+def process_semi_data(classes=["microcontroller", "timer"]):
+    """Read-in, process, and store an HFTS semiconductor dataset"""
+    datasets = [DataFrame() for _ in range(64)]
+    for c in classes:
+        files = listdir(f"data/semiconductor")
+        for f in files:
+            if c in f:
+                print(f"Reading in {f}")
+                data = read_csv(f"data/semiconductor/{f}", header=None)
+                data["label"] = c
+                for i in range(64):
+                    df = datasets[i]
+                    row = data.iloc[i]
+                    df = df._append(row, ignore_index=True)
+                    datasets[i] = df
+    # Save the list of DataFrams as a pickle file
+    p_filename = f"semi_d"
+    for c in classes: 
+        p_filename += f"_{c}"
+    p_filename += ".pkl"
+    with open(f"pickles/{p_filename}", "wb") as file:
+        dump(datasets, file)
+#==============================================================================
+if __name__ == "__main__":
+    process_semi_data()
+#==============================================================================
