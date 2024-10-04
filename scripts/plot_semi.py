37665aa23e0588740c5e2400ffbd2c8c8fcc3088
Derek Johnston <derek.a.johnston@ttu.edu>
2024-10-03 22:34:30 -0500

    Tweaks to the semi plot.

--- scripts/plot_semi.py
+++ scripts/plot_semi.py
@@ -3,6 +3,7 @@
 #==============================================================================
 from helpers import load_pickle_results
 import matplotlib.pyplot as plt
+from matplotlib.gridspec import GridSpec
 from numpy.random import normal
 from numpy import mean
 #==============================================================================
@@ -19,20 +20,26 @@ def plot_semi(classes=["microcontroller", "timer"]):
     for i in range(64):
         synth_data.append(normal(means[i], sdevs[i], 1000))
     
-    fig, ax = plt.subplots(figsize=(10, 5))
+    fig = plt.figure(figsize=(10, 3.25))
     plt.rcParams.update({"font.size": 9})
+    gs = GridSpec(1, 6, figure=fig)
+    ax0 = fig.add_subplot(gs[0, 0:4])
+    ax1 = fig.add_subplot(gs[0, 4:6])
     #ax.bar(range(len(means)), means, color="black", edgecolor="black", hatch="//", width=0.5)
-    ax.scatter(range(len(means)), means, color="black", marker="s")
-    ax.scatter(range(len(diffs)), diffs, color="dimgray", marker="_")
+    ax0.scatter(range(len(means)), means, color="black", marker="s")
+    ax0.scatter(range(len(diffs)), diffs, color="dimgray", marker="_")
     for i, (m, diff) in enumerate(zip(means, diffs)):
-        ax.vlines(x=i, ymin=diff, ymax=m, color="dimgray", linewidth=0.5)
-    ax.hlines(y=mean(means), xmin=-1, xmax=64, color="dimgray", linestyle="--", linewidth=0.5)
+        ax0.vlines(x=i, ymin=diff, ymax=m, color="dimgray", linewidth=0.5)
+    ax0.hlines(y=mean(means), xmin=-1, xmax=64, color="dimgray", linestyle="--", linewidth=0.5)
     #ax.bar(range(len(diffs)), diffs, color="white", edgecolor="dimgray", width=0.5)
-    ax.legend(["$\\mu$", "$\\mu - \\sigma$"], frameon=False, loc="upper left")
-    ax.set_xlim(-1, 64)
-    ax.set_ylim(0, 1.1)
-    ax.set_xlabel("DUT Pin Permutation")
-    ax.set_ylabel("Accuracy Score")
+    ax0.legend(["$\\mu$", "$\\mu - \\sigma$"], frameon=False, loc="upper left")
+    ax0.set_xlim(-1, 64)
+    ax0.set_ylim(0, 1.15)
+    ax0.set_xlabel("DUT Pin Permutation")
+    ax0.set_ylabel("Accuracy Score")
+    ax0.set_title("(a)")
+
+    ax1.set_title("(b)")
     plt.tight_layout()
     plt.savefig("images/semi.svg")
 
